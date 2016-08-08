bbdf4d86ff6ac2a2f713f8f4a44b84061f21ac14
DanSuh <dansuh3593@gmail.com>
2016-08-08 17:46:37 -0400

    Begin seeding method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,28 @@
+# require 'csv'
+#
+# csv_text = File.read(Rails.root.join('lib', 'seeds', 'braingain_seeds.csv'))
+# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+# csv.each do |row|
+#   t = Transaction.new
+#   t.street = row['street']
+#   t.city = row['city']
+#   t.zip = row['zip']
+#   t.zip = row['zip']
+#   t.state = row['state']
+#   t.beds = row['beds']
+#   t.sq_feet = row['sq_feet']
+#   t.category = row['type']
+#   t.sale_date = row['sale_date']
+#   t.price = row['price']
+#   t.lat = row['latitude']
+#   t.lng = row['longitude']
+#   t.save
+#   puts "#{t.street}, #{t.city} saved"
+# end
+#
+# puts "There are now #{Transaction.count} rows in the transactions table"
+
+
 # User seeds
 User.create!({ username: "john.doe", password: "dandan" }) # id: 1
 User.create!({ username: "NoRiceApproach", password: "dandan" })
