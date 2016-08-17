6e88ee313925c66d7678edbb1776495b7ef3af25
DanSuh <dansuh3593@gmail.com>
2016-08-17 10:54:40 -0400

    Remove test seed code and csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,3 @@
-# require 'csv'
-#
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'braingain_seeds.csv'))
-# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-#
-# subject_id = 0
-# deck_id = 0
-# csv.each do |row|
-#   if row['username'] == ""
-#     #deck conditional
-#     #card create
-#   else
-#   end
-
-    # t = Transaction.new
-    # t.street = row['street']
-    # t.city = row['city']
-    # t.zip = row['zip']
-    # t.zip = row['zip']
-    # t.state = row['state']
-    # t.beds = row['beds']
-    # t.sq_feet = row['sq_feet']
-    # t.category = row['type']
-    # t.sale_date = row['sale_date']
-    # t.price = row['price']
-    # t.lat = row['latitude']
-    # t.lng = row['longitude']
-    # t.save
-
-
-
-
-
 User.destroy_all
 Subject.destroy_all
 Deck.destroy_all
@@ -3105,7 +3072,7 @@ Subject.create!({ title: "Geometry", author_id: 8 })
       title: "Circles",
       description: "The shape with no corner",
       subject_id: 31
-      })
+    })
       Card.create!({
         question: "question",
         answer: "answer",
